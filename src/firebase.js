40e207963554d766d0126848aa69178ab02318bf
vishnu0921 <ba11@iitbbs.ac.in>
2022-02-06 19:57:13 +0530

    Initialized project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOoDW42p-WwPdB26RzEYxRmTqlMyxjTl8",
+    authDomain: "fb-clone-b81f0.firebaseapp.com",
+    projectId: "fb-clone-b81f0",
+    storageBucket: "fb-clone-b81f0.appspot.com",
+    messagingSenderId: "1097539503895",
+    appId: "1:1097539503895:web:6ea0042609b4d32bfd5952"
+  };

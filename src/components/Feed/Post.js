cd12204001d41646f66f6fb5768e0fee6ba27fbd
vishnu0921 <ba11@iitbbs.ac.in>
2022-02-10 10:41:04 +0530

    Added post post options

---
+++ src/components/Feed/Post.js
@@ -0,0 +1,43 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './Post.css'
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
+import NearMeOutlinedIcon from '@material-ui/icons/NearMeOutlined';
+
+function Post({profilePic, image, username, timestamp, message }) {
+  return <div className='post'>
+      <div className="post-top">
+          <Avatar src={profilePic} className='post-avatar' />
+          <div className="post-top-info">
+              <h3>{username}</h3>
+              <p>Timestamp...</p>
+          </div>
+      </div>
+
+      <div className="post-body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post-image">
+          <img src={image} alt="" />
+      </div>
+
+      <div className="react-options">
+          <div className="react-option">
+            <ThumbUpAltOutlinedIcon />
+            <p>Like</p>
+          </div>
+          <div className="react-option">
+            <ChatBubbleOutlineOutlinedIcon />
+            <p>Comment</p>
+          </div>
+          <div className="react-option">
+            <NearMeOutlinedIcon />
+            <p>Share</p>
+          </div>
+      </div>
+  </div>;
+}
+
+export default Post;

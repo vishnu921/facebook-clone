df95822a21277e5f51b1062ce866d45b5721178a
vishnu0921 <ba11@iitbbs.ac.in>
2022-02-10 19:07:45 +0530

    connection to firebase

--- src/auth/login/Login.js
+++ src/auth/login/Login.js
@@ -1,11 +1,16 @@
 import { Button } from '@material-ui/core';
 import React from 'react'
 import "./Login.css"
+import { auth, provider } from "../../firebase"
+
 
 function Login() {
 
     const signIn = () => {
-
+        auth.signInWithPopup(provider)
+        .then(result => {
+            console.log(result);
+        }).catch((error) => alert(error.message));
     }
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAOoDW42p-WwPdB26RzEYxRmTqlMyxjTl8",
     authDomain: "fb-clone-b81f0.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
     messagingSenderId: "1097539503895",
     appId: "1:1097539503895:web:6ea0042609b4d32bfd5952"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;

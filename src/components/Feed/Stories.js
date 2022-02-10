0d03f096efa65415de325ceb03bba29d25f96f08
vishnu0921 <ba11@iitbbs.ac.in>
2022-02-10 20:56:46 +0530

    refractored css

--- src/components/Feed/Stories.js
+++ src/components/Feed/Stories.js
@@ -13,31 +13,31 @@ function Stories() {
   return <div className='stories'>
         <StoryItem 
             image="https://scontent.fmaa8-1.fna.fbcdn.net/v/t15.5256-10/s235x165/273715982_466206781666588_8960397218121726075_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ad6a45&_nc_ohc=mKdJ11RMHtUAX9gMNw2&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_sMNiI9kb8RwLrRTe3DLBmKWD5ZB9O3JjakkxRO0D6Eg&oe=6207DED5"
-            profilePic="https://scontent.fmaa8-1.fna.fbcdn.net/v/t1.6435-1/cp0/c0.0.60.60a/p60x60/132413134_432407187787338_919365767583361061_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=IZebQoa9P64AX_7od95&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_aXPx4p15UzxOHSb2Uk6f9NElI1Sk8UzkHCilrltAc4w&oe=6228F3D4"
-            title="Ishan Sinha"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBHyHdxC1Ha91JQ3kHfUv8ig2wm57Pp8lVg&usqp=CAU"
+            title="Virat Kohli"
         />
         
         <StoryItem 
             image="https://scontent.fmaa8-1.fna.fbcdn.net/v/t15.5256-10/s235x165/273715982_466206781666588_8960397218121726075_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ad6a45&_nc_ohc=mKdJ11RMHtUAX9gMNw2&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_sMNiI9kb8RwLrRTe3DLBmKWD5ZB9O3JjakkxRO0D6Eg&oe=6207DED5"
-            profilePic="https://scontent.fmaa8-1.fna.fbcdn.net/v/t1.6435-1/cp0/c0.0.60.60a/p60x60/132413134_432407187787338_919365767583361061_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=IZebQoa9P64AX_7od95&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_aXPx4p15UzxOHSb2Uk6f9NElI1Sk8UzkHCilrltAc4w&oe=6228F3D4"
-            title="Ishan Sinha"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBHyHdxC1Ha91JQ3kHfUv8ig2wm57Pp8lVg&usqp=CAU"
+            title="Virat Kohli"
         />
         
         <StoryItem 
             image="https://scontent.fmaa8-1.fna.fbcdn.net/v/t15.5256-10/s235x165/273715982_466206781666588_8960397218121726075_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ad6a45&_nc_ohc=mKdJ11RMHtUAX9gMNw2&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_sMNiI9kb8RwLrRTe3DLBmKWD5ZB9O3JjakkxRO0D6Eg&oe=6207DED5"
-            profilePic="https://scontent.fmaa8-1.fna.fbcdn.net/v/t1.6435-1/cp0/c0.0.60.60a/p60x60/132413134_432407187787338_919365767583361061_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=IZebQoa9P64AX_7od95&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_aXPx4p15UzxOHSb2Uk6f9NElI1Sk8UzkHCilrltAc4w&oe=6228F3D4"
-            title="Ishan Sinha"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBHyHdxC1Ha91JQ3kHfUv8ig2wm57Pp8lVg&usqp=CAU"
+            title="Virat Kohli"
         />
         
         <StoryItem 
             image="https://scontent.fmaa8-1.fna.fbcdn.net/v/t15.5256-10/s235x165/273715982_466206781666588_8960397218121726075_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ad6a45&_nc_ohc=mKdJ11RMHtUAX9gMNw2&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_sMNiI9kb8RwLrRTe3DLBmKWD5ZB9O3JjakkxRO0D6Eg&oe=6207DED5"
-            profilePic="https://scontent.fmaa8-1.fna.fbcdn.net/v/t1.6435-1/cp0/c0.0.60.60a/p60x60/132413134_432407187787338_919365767583361061_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=IZebQoa9P64AX_7od95&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_aXPx4p15UzxOHSb2Uk6f9NElI1Sk8UzkHCilrltAc4w&oe=6228F3D4"
-            title="Ishan Sinha"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBHyHdxC1Ha91JQ3kHfUv8ig2wm57Pp8lVg&usqp=CAU"
+            title="Virat Kohli"
         />
         
         <StoryItem 
             image="https://scontent.fmaa8-1.fna.fbcdn.net/v/t15.5256-10/s235x165/273715982_466206781666588_8960397218121726075_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ad6a45&_nc_ohc=mKdJ11RMHtUAX9gMNw2&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_sMNiI9kb8RwLrRTe3DLBmKWD5ZB9O3JjakkxRO0D6Eg&oe=6207DED5"
-            profilePic="https://scontent.fmaa8-1.fna.fbcdn.net/v/t1.6435-1/cp0/c0.0.60.60a/p60x60/132413134_432407187787338_919365767583361061_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=IZebQoa9P64AX_7od95&_nc_ht=scontent.fmaa8-1.fna&oh=00_AT_aXPx4p15UzxOHSb2Uk6f9NElI1Sk8UzkHCilrltAc4w&oe=6228F3D4"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBHyHdxC1Ha91JQ3kHfUv8ig2wm57Pp8lVg&usqp=CAU"
             title="Ishan Sinha"
         />
   </div>;
--- src/components/Feed/Widgets.js
+++ src/components/Feed/Widgets.js
@@ -4,7 +4,8 @@ import './Widgets.css'
 function Widgets() {
   return (
     <div className='widgets'>
-        <iframe 
+        <iframe
+            title='random-iframe'
             src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FProgrammersCreateLife%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
             width="340" 
             height="100%" 
